9252b4f5f5903d59feb8d5558b6cd388abcbc519
sow4 <waldeksowa@o2.pl>
2021-03-01 15:00:11 +0100

    initial commit

---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcTest {
+
+    @Test
+    void shouldProperAdd() {
+        Calc calc = new Calc();
+
+        int result = calc.add(2,3);
+
+        assertEquals(5, result);
+    }
+
+    @Test
+    void shouldProperSub() {
+        Calc calc = new Calc();
+
+        int result = calc.sub(6,3);
+
+        assertEquals(3, result);
+    }
+}
